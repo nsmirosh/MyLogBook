52a6d050c2ba2151316b20814f3892b17a4e04d4
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2024-01-19 20:07:51 +0700

    The data is being written and returned from the database

--- app/src/main/java/nick/mirosh/logbook/domain/usecase/SaveBloodMeasurementUseCase.kt
+++ app/src/main/java/nick/mirosh/logbook/domain/usecase/SaveBloodMeasurementUseCase.kt
@@ -1,13 +1,19 @@
 package nick.mirosh.logbook.domain.usecase
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import nick.mirosh.logbook.data.repositories.BloodMeasurementsRepository
+import nick.mirosh.logbook.di.IoDispatcher
 import nick.mirosh.logbook.domain.model.BmEntry
 import javax.inject.Inject
 
 class SaveBloodMeasurementUseCase @Inject constructor(
-    private val bloodMeasurementsRepository: BloodMeasurementsRepository
+    private val bloodMeasurementsRepository: BloodMeasurementsRepository,
+    @IoDispatcher private val dispatcher: CoroutineDispatcher
 ) {
     suspend operator fun invoke(bmEntry: BmEntry) {
-        bloodMeasurementsRepository.saveEntry(bmEntry)
+        withContext(dispatcher) {
+            bloodMeasurementsRepository.saveEntry(bmEntry)
+        }
     }
 }
